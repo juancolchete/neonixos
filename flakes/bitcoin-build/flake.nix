eaac81730d0635d238abb35c6d16a5126b858b3b
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-10-07 10:45:19 -0300

    chore: add a working bitcoin build flake

--- flakes/bitcoin-build/flake.nix
+++ flakes/bitcoin-build/flake.nix
@@ -1,72 +1,35 @@
 {
-  description = "A Nix-flake-based C/C++ development environment";
+  description = "Bitcoin Core build environment (CMake)";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
-  inputs.nixpkgs_boost.url = "github:nixos/nixpkgs/35e296f5e55a85a815ae22fb81259edb3a2d28d1";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
 
-  outputs = inputs:
+  outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs {
-		inherit system;
-		overlays = [
-			(final: prev: {
-          pin-boost = import inputs.nixpkgs_boost {
-            inherit (final) system;
-            config.allowUnfree = true;
-          };
-		})
-		];
-      };});
-      rootOfShell = "./";
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }:
-      let
-          mkWrapper = ({cmd, pkg}: pkgs.writeShellScriptBin "${cmd}" ''
-            ${pkgs.direnv}/bin/direnv exec ${rootOfShell} ${pkg}/bin/${cmd} "$@"
-          '');
-          wrappers = [
-              (mkWrapper {cmd = "cmake"; pkg = pkgs.cmake;})
-              (mkWrapper {cmd = "ninja"; pkg = pkgs.ninja;})
-              (mkWrapper {cmd = "gcc"; pkg = pkgs.gcc;})
-              (mkWrapper {cmd = "g++"; pkg = pkgs.gcc;})
-              (mkWrapper {cmd = "gdb"; pkg = pkgs.gdb;})
-              (mkWrapper {cmd = "valgrind"; pkg = pkgs.valgrind;})
-          ];
-      in {
-        default = pkgs.mkShell.override
-          {
-            # Override stdenv in order to change compiler:
-            # stdenv = pkgs.clangStdenv;
-          }
-          {
-            packages = wrappers ++ (with pkgs; [
-              clang-tools
-              codespell
-              conan
-              cppcheck
-              doxygen
-              gtest
-              lcov
-              vcpkg
-              vcpkg-tool
-	      pipewire.dev
-              fftwFloat
-	      pkgconf
-	      pin-boost.boost173.dev
-              sqlite.dev
-              libevent.dev
-              miniupnpc
-	      libnatpmp
-              qrencode
-            ]);
-           CMAKE_PREFIX_PATH = "${pkgs.pin-boost.boost173.dev}";
-           BOOST_ROOT = "${pkgs.pin-boost.boost173.dev}";
-          };
-      });
-    };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "bitcoin-dev-env";
 
-  
+        buildInputs = with pkgs; [
+          cmake
+          boost
+          libevent
+          zeromq
+          zlib
+          pkg-config
+          capnproto
+          python3
+          sqlite
+        ];
+
+        shellHook = ''
+          export BOOST_ROOT=${pkgs.boost}
+          export BOOST_INCLUDEDIR=${pkgs.boost}/include
+          export BOOST_LIBRARYDIR=${pkgs.boost}/lib
+        '';
+      };
+    };
 }
